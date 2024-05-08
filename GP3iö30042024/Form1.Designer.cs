6e4cd374d7d66f9108c485b60fd49805d2df8794
Doğukan ÖZEL <dogukanozel692@gmail.com>
2024-05-08 11:56:57 +0300

    Add project files.

---
+++ GP3iö30042024/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace GP3iö30042024
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHastaAd = new System.Windows.Forms.TextBox();
+            this.txtHastaTc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRandevuAl = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.DisplayMember = "pol";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(89, 108);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.ValueMember = "polno";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Poliklinikler:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.DisplayMember = "doktor";
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(108, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(88, 108);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.ValueMember = "doktorno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Doktorlar:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(203, 33);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(409, 33);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(73, 108);
+            this.listBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(406, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Saat:";
+            // 
+            // txtHastaAd
+            // 
+            this.txtHastaAd.Location = new System.Drawing.Point(268, 72);
+            this.txtHastaAd.Name = "txtHastaAd";
+            this.txtHastaAd.Size = new System.Drawing.Size(135, 20);
+            this.txtHastaAd.TabIndex = 5;
+            // 
+            // txtHastaTc
+            // 
+            this.txtHastaTc.Location = new System.Drawing.Point(269, 98);
+            this.txtHastaTc.Name = "txtHastaTc";
+            this.txtHastaTc.Size = new System.Drawing.Size(134, 20);
+            this.txtHastaTc.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hasta Ad : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Hasta TC :,";
+            // 
+            // btnRandevuAl
+            // 
+            this.btnRandevuAl.Location = new System.Drawing.Point(328, 124);
+            this.btnRandevuAl.Name = "btnRandevuAl";
+            this.btnRandevuAl.Size = new System.Drawing.Size(75, 23);
+            this.btnRandevuAl.TabIndex = 7;
+            this.btnRandevuAl.Text = "Randevu Al";
+            this.btnRandevuAl.UseVisualStyleBackColor = true;
+            this.btnRandevuAl.Click += new System.EventHandler(this.btnRandevuAl_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 191);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 208);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 423);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRandevuAl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHastaTc);
+            this.Controls.Add(this.txtHastaAd);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Randevu AL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHastaAd;
+        private System.Windows.Forms.TextBox txtHastaTc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRandevuAl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ GP3iö30042024/metodlar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;//ekle
+
+namespace GP3iö30042024
+{
+    internal class metodlar
+    {
+        SqlConnection bag = new SqlConnection("server=DESKTOP-MJGGV3B;initial catalog=hastanerandevu;integrated security=true");
+        public void lbyukle(ListBox lb,string sql)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(sql, bag);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            lb.DataSource = dt;
+            lb.DisplayMember = dt.Columns[1].ColumnName;//"polno","doktorno,"saatno
+            lb.ValueMember = dt.Columns[0].ColumnName;// "polno". . . ;
+        }public void dgvyukle(DataGridView dgv)
+        {
+            string sql = "select randevular.randevuno,randevular.hasta,randevular.tc,randevular.doktorno,randevular.tarih,saatler.saatler from randevular INNER JOIN saatler on randevular.saatno=saatler.saatno";
+            SqlDataAdapter da = new SqlDataAdapter(sql, bag);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgv.DataSource = dt;
+            dgv.Columns[0].HeaderText = "Randevu No";
+            dgv.Columns[1].HeaderText = "Hastaa Adı";
+            dgv.Columns[2].HeaderText = "Hasta TC";
+            dgv.Columns[3].HeaderText = "Doktor NO";
+            dgv.Columns[4].HeaderText = "Randevu Tarih ";
+            
+            
+            
+        }
+        public void randevual(string hasta,string tc,string doktorno,string tarih,string saatno)
+        {
+            string sql = "insert into randevular (hasta,tc,doktorno,tarih,saatno) values ('" + hasta + "','" + tc + "','" + doktorno + "','" + tarih + "','" + saatno + "')";
+            SqlCommand cmd = new SqlCommand(sql,bag);
+            bag.Open();
+            cmd.ExecuteNonQuery();
+            bag.Close();
+            MessageBox.Show("Kaydedildi","Randevu Kayıt",MessageBoxButtons.YesNo,MessageBoxIcon.Asterisk);
+            
+        }
+
+    }
+}
